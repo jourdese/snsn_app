0f37b252bae2a9e8a716436080bda88b4c28eebe
Jour <jourdesepalacio@gmail.com>
2025-05-08 05:08:14 +0800

    Setup complete

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const Index = () => {
+    return (
+        <View>
+            <Text>Index</Text>
+        </View>
+    )
 }
+export default Index
+const styles = StyleSheet.create({})
