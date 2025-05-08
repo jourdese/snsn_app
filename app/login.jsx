2a58db89d457484fd61e8ee6259fa49262b7f657
Jour <jourdesepalacio@gmail.com>
2025-05-09 03:10:45 +0800

    added icon

--- app/login.jsx
+++ app/login.jsx
@@ -1,10 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native'
 import React from 'react'
+import Icon from "../assets/icons";
+import {theme} from "../constants/theme";
 
 const Login = () => {
     return (
         <View>
             <Text>Login</Text>
+            {/* eslint-disable-next-line no-undef */}
+            <Icon name="home" />
         </View>
     )
 }
