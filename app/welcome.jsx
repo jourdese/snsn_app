af075e0bdd12c2b242929b8075922b88f70a1f4f
Jour <jourdesepalacio@gmail.com>
2025-05-09 02:37:14 +0800

    added route for login and signup

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -4,8 +4,10 @@ import ScreenWrapper from "../components/ScreenWrapper";
 import {hp, wp} from "../helpers/common";
 import {theme} from "../constants/theme";
 import Button from "../components/Button";
+import {useRouter} from "expo-router";
 
 const Welcome = () => {
+    const router = useRouter();
     return (
         <ScreenWrapper bg="white">
             <StatusBar style="dark"/>
@@ -28,15 +30,18 @@ const Welcome = () => {
                     <Button
                         title="Getting Started"
                         buttonStyle={{marginHorizontal: wp(3)}}
-                        onPress={() => {
-                        }}
+                        onPress={() => router.push("./signup")
+                        }
                     />
                     <View style={styles.bottomTextContainer}>
                         <Text style={styles.loginText}>
                             Already have an account!
                         </Text>
-                        <Pressable>
-                            <Text style={[styles.loginText, {color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold}]}>
+                        <Pressable onPress={() => router.push("./login")}>
+                            <Text style={[styles.loginText, {
+                                color: theme.colors.primaryDark,
+                                fontWeight: theme.fonts.semibold
+                            }]}>
                                 Login
                             </Text>
                         </Pressable>
