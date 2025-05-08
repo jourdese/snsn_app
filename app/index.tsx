8771753fa6dc5a7a7cfbf44f3949b53772505ecf
Jour <jourdesepalacio@gmail.com>
2025-05-09 05:49:16 +0800

    added input validation and alert

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,17 @@
-import {View, Text, Button} from 'react-native'
+import {Button, Text} from 'react-native'
 import React from 'react'
 import {useRouter} from 'expo-router'
 import ScreenWrapper from "@/components/ScreenWrapper";
 
+
 const index = () => {
     const router = useRouter();
     return (
-        <ScreenWrapper>
+        <ScreenWrapper bg="white">
             <Text>index</Text>
             <Button title="welcome" onPress={() => router.push('./welcome')}/>
         </ScreenWrapper>
-    );
+    )
 }
 
 export default index
--- components/BackButton.jsx
+++ components/BackButton.jsx
@@ -4,16 +4,16 @@ import Icon from "../assets/icons";
 import {theme} from "../constants/theme";
 import {router} from "expo-router";
 
-const BackButton = ({size=26, router}) => {
+const BackButton = ({size = 26, router}) => {
     return (
-        <Pressable onPress={()=>router.back()} style={styles.button}>
-            <Icon name="arrowLeft" strokeWidth={2.5} size={size} color={theme.colors.text} />
+        <Pressable onPress={() => router.back()} style={styles.button}>
+            <Icon name="arrowLeft" strokeWidth={2.5} size={size} color={theme.colors.text}/>
         </Pressable>
     )
 }
 export default BackButton
 const styles = StyleSheet.create({
-    button:{
+    button: {
         alignSelf: 'flex-start',
         padding: 5,
         borderRadius: theme.radius.sm,
