1548ae1b1c3475e0609d4cc239cad565c80ce2f9
Jour <jourdesepalacio@gmail.com>
2025-05-09 03:30:32 +0800

    added backbutton

--- app/login.jsx
+++ app/login.jsx
@@ -1,15 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native'
+import {StatusBar, StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 import Icon from "../assets/icons";
 import {theme} from "../constants/theme";
+import ScreenWrapper from "../components/ScreenWrapper";
+import BackButton from "../components/BackButton";
+import {useRouter} from "expo-router";
 
 const Login = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>Login</Text>
-            {/* eslint-disable-next-line no-undef */}
-            <Icon name="home" />
-        </View>
+        <ScreenWrapper>
+            <StatusBar barStyle="light-content" />
+            <View style={styles.container}>
+                <BackButton router={router}/>
+            </View>
+        </ScreenWrapper>
     )
 }
 export default Login
